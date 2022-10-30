81d85baeb9aa34bb7f3c75026f6737d33c74904b
zjooy <zjoy_@outlook.com>
2022-10-30 18:10:18 -0300

    Alteração do Header e Footer padrão

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 **Project Developer 2022G**
 
 <em>30/10/22</em> Projeto criado e enviado para o repositório.
+<em>30/10/22</em> Alteração do Header e Footer padrão:
+em _Layout.cshtml foram adicionados novos itens de lista e o menu fixo a fim de testes,
+foi adicionado id para estilizações do header na pasta root/site.css,
+adicionado um dropDown para o menu na pasta root/site.js
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// dropDown menu
+$(function () {
+    $('.navbar li').hover(
+        function () {
+            $('>ul.sub:not(:animated)', this).slideDown(500);
+        },
+        function () {
+            $('>ul.sub', this).slideUp(200);
+        },
+    );
+});
